a6726b6710480d060f128c82c8eab742bf9672c3
OQ <zfdok@126.com>
2020-10-27 14:11:45 +0800

    add post

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,22 @@
-const router = require('koa-router')()
+const router = require("koa-router")();
 
-router.get('/', async (ctx, next) => {
-  ctx.body = ctx.request.query.msg
-})
+router.get("/", async (ctx, next) => {
+  ctx.body = ctx.request.query.msg;
+});
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
+router.post("/", async (ctx, next) => {
+  let postdata = ctx.request.body; //注意是异步调用
+  ctx.body = postdata;
+});
 
-router.get('/json', async (ctx, next) => {
+router.get("/string", async (ctx, next) => {
+  ctx.body = "koa2 string";
+});
+
+router.get("/json", async (ctx, next) => {
   ctx.body = {
-    title: 'koa2 json'
-  }
-})
+    title: "koa2 json",
+  };
+});
 
-module.exports = router
+module.exports = router;
